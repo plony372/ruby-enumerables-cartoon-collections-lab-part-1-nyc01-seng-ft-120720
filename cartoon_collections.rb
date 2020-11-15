83542cc94bcde82bda8d03b42db1066561c54b5e
plony benav <menachemfuterfas@gmail.com>
2020-11-15 00:14:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
 array.each do |oppressed_worker|
-   puts "#{oppressed_worker.capitalize} wants to start a union!"
+   puts "Hello #{oppressed_worker}"
 end
 
 def list_dwarves(array)
